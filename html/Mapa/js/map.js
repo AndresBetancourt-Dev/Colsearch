cf49b27a3d956e73f08deced872ac27b2c3bd6f5
AndresBetancourt-Dev <contacto.betancourth@gmail.com>
2020-09-12 13:44:44 -0500

    Finished partially the views of the webpage

---
+++ html/Mapa/js/map.js
@@ -0,0 +1,5 @@
+mapboxgl.accessToken = 'pk.eyJ1IjoiYW5kcmVzYmV0YW5jb3VydCIsImEiOiJja2V6eWxvbGQwZzh3MnlsYzZ0azBtM3huIn0.JybMBeDUjT8F9oGXG3OdFg';
+var map = new mapboxgl.Map({
+  container: 'map',
+  style: 'mapbox://styles/mapbox/streets-v11'
+});
